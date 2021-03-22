29a13fe09f6e050ccd8600359a04975b3da66ff5
vjperez <vijapefi@gmail.com>
2021-03-21 22:27:55 -0400

    random choice using randrange from random

--- randomChoice.py
+++ randomChoice.py
@@ -2,14 +2,14 @@
 
 import random
 
-#seq = [ x    for x in range(1,5)]
-#seq = [ 1+x  for x in range(4)]
-seq = [1, 2, 3, 4]
+seq = [101, 200, 333, 407]
+
 for i in range(20):
-    print('random choice from 1 to 4: = ', random.choice( seq ))
+    print('random choice from seq: = ', random.choice( seq ))
 print('\n\n')
 
 # using randrange, to implement my version of choice
 for i in range(20):
-    print('random choice from  1 to 4 using randrange: = ', random.randrange(1,5))
+    indiceRandom = random.randrange(4)
+    print('random choice from  seq using randrange: = ', seq[indiceRandom])
 print('\n\n')
