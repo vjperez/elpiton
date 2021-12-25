c0ada865ffe38cd21fcfea0c78cef459fb2321b2
victor j. perez <vijapefi@gmail.com>
2021-12-25 12:03:48 -0400

    factores as getFactors function and importing printeaSeq

--- factores02.py
+++ factores02.py
@@ -1,12 +1,28 @@
-n = 36
+import printeaSeq
 
-#using comprehension syntax: list comprehension
-factors = [isFactor for isFactor in range(1, n+1) if n % isFactor == 0]
 
-for i in range(len(factors)):
-    print(factors[i])
+def getFactors(v):
+    if isinstance(v, int) and v > 1:
+        #using comprehension syntax: list comprehension
+        factors = [isFactor for isFactor in range(1, v+1) if v % isFactor == 0]
+        return factors
+    else: 
+        raise Exception('argumento no es int mayor q 1.')
 
-print('\n\n')
 
-for factor in factors:
-    print(factor)
+
+if __name__ == '__main__':
+    print('2 ways to print factors of 24')
+    x = 24
+    f = getFactors(x)
+    for i in range(len( f )):
+        print( f[i] )
+
+    print('\n\n')
+
+    for factor in f:
+        print(factor, '          ', sep='', end='')
+
+    print('\n\ntercera forma .... importing la funcion printeaSeq')
+    printeaSeq.printeaSeq('factores de 24:', f)
+
